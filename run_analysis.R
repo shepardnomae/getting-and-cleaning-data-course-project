35c8ce9c3a1ed53a31f3f94aa06a29339070df86
BuhiBuhi <tongkimu@ToNgs-MacBook-Pro.local>
2021-02-18 23:45:38 +0900

    1st attemp, 5 steps completed

---
+++ run_analysis.R
@@ -0,0 +1,21 @@
+## Merges the training and the test sets to create one data set.
+## First create a directory called "mydata"
+if(!dir.exists("./mydata")){dir.create("./mydata")}
+
+
+## Extracts only the measurements on the mean and standard deviation for each measurement. 
+
+
+
+
+## Uses descriptive activity names to name the activities in the data set
+
+
+
+
+## Appropriately labels the data set with descriptive variable names. 
+
+
+
+
+## From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
