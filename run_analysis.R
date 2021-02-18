33d263b67bd9fdd78b64e5de693e5b611134a61e
BuhiBuhi <tongkimu@ToNgs-MacBook-Pro.local>
2021-02-19 01:17:33 +0900

    added col name gsub, to make my_answer more descriptive

--- run_analysis.R
+++ run_analysis.R
@@ -32,30 +32,42 @@ Merged_Data <- cbind(Subject, Y, X)
 
 print("1st step completed")
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-## locate mean() and std()
-header.mean <- grep("mean()", headername)
-header.std <- grep("std()", headername)
-## combine testortrain, mean(), stg()
-my_extract <- select(subject, code, contains("mean"), contains("std"))
+
+## locate mean() and std() combinemean(), stg()
+my_extract <- select(Merged_Data, subject, code, contains("mean"), contains("std"))
 print("2nd step completed")
 # 3. Uses descriptive activity names to name the activities in the data set
 ## replace activity label 1:6 with descriptive label names, first load activity_labels.txt
-activity_labels <- read.table("./mydata/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-my_subbed_names = gsub("(1$)", activity_labels[1,2], my_extract_names)
-my_extract3 <- `names<-`(my_extract, my_subbed_names)
+my_extract$code <- activities[my_extract$code, 2]
 print("3rd step completed")
 
 # 4. Appropriately labels the data set with descriptive variable names. 
 ## uniform cases
-mixed_case_names <- names(my_extract3)
+names(my_extract)[2] = "activity"
+names(my_extract)<-gsub("Acc", "Accelerometer", names(my_extract))
+names(my_extract)<-gsub("Gyro", "Gyroscope", names(my_extract))
+names(my_extract)<-gsub("BodyBody", "Body", names(my_extract))
+names(my_extract)<-gsub("Mag", "Magnitude", names(my_extract))
+names(my_extract)<-gsub("^t", "Time", names(my_extract))
+names(my_extract)<-gsub("^f", "Frequency", names(my_extract))
+names(my_extract)<-gsub("tBody", "TimeBody", names(my_extract))
+names(my_extract)<-gsub("-mean()", "Mean", names(my_extract), ignore.case = TRUE)
+names(my_extract)<-gsub("-std()", "STD", names(my_extract), ignore.case = TRUE)
+names(my_extract)<-gsub("-freq()", "Frequency", names(my_extract), ignore.case = TRUE)
+names(my_extract)<-gsub("angle", "Angle", names(my_extract))
+names(my_extract)<-gsub("gravity", "Gravity", names(my_extract))
+
+mixed_case_names <- names(my_extract)
 lowered <- tolower(mixed_case_names)
-my_extract4 <- `names<-`(my_extract3, lowered)
+my_extract4 <- `names<-`(my_extract, lowered)
 print("4th step completed")
 
 ## From the data set in step 4, creates a second, independent tidy data set with
 ## the average of each variable for each activity and each subject.
-cran_my_extract <- tbl_df(my_extract4)
-my_anwser <- sapply(my_extract, mean)
+my_answer <- my_extract4 %>%
+  group_by(subject, activity) %>%
+  summarise_all(funs(mean))
+write.table(my_answer, "my_answer.txt", row.name=FALSE)
 
 
 
